8768c66df494ad5b190406adb216471781506b39
Netta Avidan <nettavidan19@gmail.com>
2021-06-08 11:59:23 +0300

    -

--- app/controllers/productsController.js
+++ app/controllers/productsController.js
@@ -1,4 +1,8 @@
-function ProductsController($scope){
-    $scope.products= [{name:'netta', city:'haifa'},{name:'aya', city:'carmiel'},'tully']
-}
+(function (){
+    var ProductsController = function($scope){
+        $scope.products= [{name:'netta', city:'haifa'},{name:'aya', city:'carmiel'},'tully']
+    }
+
+    angular.module('shopApp').controller('ProductsController', ProductsController)
+})
 //ProductsController()
